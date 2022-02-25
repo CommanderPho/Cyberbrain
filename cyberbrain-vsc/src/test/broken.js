6883fa7374c7996c949a0ad28803d9afb09151b6
Victor Sun <victorjsun@gmail.com>
2022-02-25 14:24:28 -0500

    Use old file

--- cyberbrain-vsc/src/test/broken.js
+++
@@ -1,8 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-const proc = require("child_process");
-const childProcess = proc.spawnSync("pdm", ["info"]);
-if (childProcess.stdout.toString() == "") {
-  console.log("Failed");
-} else {
-  console.log(childProcess.stdout.toString());
-}
